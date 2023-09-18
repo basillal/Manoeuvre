061014e80b175fff0b702673c483d871c64e6bc3
johnpaulreju <johnpaulreju2k@gmail.com>
2023-09-18 17:28:29 +0530

    few done

--- application/urls.py
+++ application/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('index/',views.index, name="index"),
-    # path('registration/', views.registrationpage_view, name='registrationpage_view'),
+    path('',views.index, name="index"),
+    path('registration/', views.registrationpage_view, name='registrationpage_view'),
     path('', views.registration_view, name='registration_view'),
     path('success/', views.success_page, name='success_page')  # Create a success page view
 
